c9be153c77faf4f7741c70e27e2ecb544df3f0ec
mattruzzi <68619790+mattruzzi@users.noreply.github.com>
2020-07-24 11:52:25 -0700

    First menu

---
+++ tapemaker.py
@@ -0,0 +1,10 @@
+print('What would you like to do?')
+print('1. Generate tape from file')
+print('2. Generate tape from pasted text')
+choices = ['1', '2']
+choice = '0'
+choice = input('Please type 1 or 2 and press Enter ')
+if choice == "1":
+
+elif choice == '2':
+    
